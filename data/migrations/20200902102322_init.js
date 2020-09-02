04e7bffcff13a1471efeda3e1941297a3b4842a3
lateralcreativity <lateralcreativity@outlook.com>
2020-09-02 10:54:10 -0500

    db setup and api running

---
+++ data/migrations/20200902102322_init.js
@@ -0,0 +1,31 @@
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('projects', table => {
+        table.increments();
+        table.string('name').notNullable().index();
+        table.string('description');
+        table.boolean('completed').notNullable().defaultTo(0)
+    })
+
+    .createTable('resources', table => {
+        table.increments();
+        table.string('name').notNullable().unique().index();
+        table.string('description')
+    })
+
+    .createTable('tasks', table => {
+        table.increments();
+        table.string('description').notNullable();
+        table.string('notes');
+        table.boolean('completed').notNullable().defaultTo(0);
+        table.integer('projects_id').unsigned()
+        .references('projects.id').onDelete('RESTRICT').onUpdate('CASCADE');
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('projects')
+};
